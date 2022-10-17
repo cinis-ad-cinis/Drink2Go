1176803a86dabdbde8104bbc996cb8be299f4408
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2022-10-17 03:39:31 +0300

    final fix

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,14 +1,14 @@
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
   loop: true,
 
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
